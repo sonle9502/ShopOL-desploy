30f96b848af8ce0d68ca482cfc321d9e6dca7948
sonle9502 <114076484+sonle9502@users.noreply.github.com>
2024-12-12 16:48:05 +0900

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
 // src/config.js
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend:5000";
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://18.181.213.99:5000";
 
